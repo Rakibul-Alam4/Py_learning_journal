2318a6f03a2038083bdcaa9e9097fffec3c23270
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-11-03 10:14:35 +0600

    Add files via upload

---
+++ control flow/circuot_evalution.py
@@ -0,0 +1,10 @@
+"""high_income = True
+good_credit = True
+student = True
+if high_income and good_credit or student:
+    print("eligible")
+    """
+
+age = 22
+if 18 <=  age < 65:
+    print("Eligible")
