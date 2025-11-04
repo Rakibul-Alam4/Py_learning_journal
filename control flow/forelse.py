1ffa8ee1a7f04cb571afdf033d1e72e8d62c605a
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-11-04 10:17:02 +0600

    Add files via upload

---
+++ control flow/forelse.py
@@ -0,0 +1,9 @@
+Successfull = True
+for Attemp in range(3):
+    print("Attemp")
+    if Successfull:
+        print("Successful")
+        break
+
+else:
+    print("3 times attemp will be failed")
