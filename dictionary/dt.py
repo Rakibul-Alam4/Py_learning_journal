6c5bf7b3c6a90915d79aad48e0ff0ee256cb0688
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-11-08 10:47:53 +0600

    Add files via upload

---
+++ dictionary/dt.py
@@ -0,0 +1,16 @@
+man = {
+    "name": "Mickel",
+    "age": 22,
+    "id": 1212
+}
+
+woman = {
+    "name":"Roy",
+    "age":22,
+    "id":1122
+}
+
+print(man,'\n',woman)
+
+man ["home"] = "cumilla"
+print (man)
