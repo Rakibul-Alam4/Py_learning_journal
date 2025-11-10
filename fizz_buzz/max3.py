62a837e82506825da14d6eac9820379ee6b92b8b
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-11-10 11:05:10 +0600

    Add files via upload

---
+++ fizz_buzz/max3.py
@@ -0,0 +1,9 @@
+def max_nums (a,b,c):
+    if a>b and a>c:
+        print("A is the biggest")
+    elif b>a and b>c:
+        print("B is the biggest")
+    elif c>b and c>a:
+        print("C is the biggest")
+
+max_nums(10,44,22)
