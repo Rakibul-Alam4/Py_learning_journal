c306293201a09c09c35933edbce6bcc2a1383f57
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-11-11 11:20:25 +0600

    greet function

---
+++ function/greet1.py
@@ -0,0 +1,21 @@
+"""
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome to the abroaad")
+
+
+greet("Adnan","Khan")
+greet("Jessika","Roy")
+"""
+
+"""
+def increment(number, by):
+    return number + by
+
+
+print(increment(2, by=1))
+"""
+def save_user(**user):
+    print(user)
+
+ 
