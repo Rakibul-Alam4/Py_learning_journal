aab8ea71dc53b9ffcb341a6e215f58573e10508f
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-11-13 11:24:29 +0600

    formatestring shortcut

---
+++ intro/str.py
@@ -0,0 +1,10 @@
+course = " Python program"
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip()) #remove space
+print(course.find("pro")) # index number of pro
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)  
