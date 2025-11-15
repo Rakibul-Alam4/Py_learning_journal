a6b6d319c200a5f41ab3010394bf9fe3ab45ebbe
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-11-15 11:14:37 +0600

    Add files via upload

---
+++ list/multi.py
@@ -0,0 +1,11 @@
+num1 = [2, 4, 6]
+num2 = [1, 3, 5]
+
+mul = [] 
+
+for i in range(len(num1)):
+    mul.append(num1[i] * num2[i])
+
+print("Num1:", num1)
+print("Num2:", num2)
+print("Multiplication result:", mul)
