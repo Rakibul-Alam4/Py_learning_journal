5a35fc9c6fa727e3ce4ca88f2e0b6533c328a0ad
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-11-15 11:14:12 +0600

    Add files via upload

---
+++ list/index.py
@@ -0,0 +1,12 @@
+numbers = ['a', 'v', 'd']
+value = [1, 4, 5, 6]
+value.append(8)
+value.append(22)
+value.pop()
+value.pop()
+value.pop()
+value.append(44)
+value.insert(1,10)
+combind = numbers+value
+
+print(combind)
