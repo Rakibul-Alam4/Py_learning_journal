f2393b6428a5c047ffb00dd551b9ce321a0a6fbf
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-11-18 10:32:15 +0600

    Add files via upload

---
+++ Stacks/browser.py
@@ -0,0 +1,15 @@
+browsing_session = []
+
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+browsing_session.append(4)
+
+print(browsing_session)
+
+
+if browsing_session.pop():
+    print("redirect : ",browsing_session[-1])
+
+print(browsing_session)
+
